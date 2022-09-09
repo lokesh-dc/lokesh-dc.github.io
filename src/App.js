37decf4368ea9c13988eccf61ec604a434485f1d
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-09-09 22:37:55 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <h1>HELLO</h1>
       <h2>Lokesh Choudhary</h2>
+      <h3>Web Developer</h3>
+      <p>Site is in progress...</p>
     </div>
   );
 }
