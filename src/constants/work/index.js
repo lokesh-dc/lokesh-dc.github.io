d7fb0a2bc1397c0c8ca5dd95b5a3886d7718a93a
‘Lokesh <lokesh.cdewanand@gmail.com>
2024-12-11 11:36:54 +0530

    Projects : Features section

--- src/constants/work/index.js
+++ src/constants/work/index.js
@@ -8,15 +8,14 @@ export const data = [
 		desc: `A dynamic and intuitive movie database exploration platform designed to help users discover movies and series effortlessly. Built with Next.js and Tailwind CSS, StreamXplore offers real-time data, advanced search features, and a seamless browsing experience. Whether you're hunting for the latest releases or searching for hidden gems, StreamXplore delivers a fast and user-friendly interface to make finding your next favorite title a breeze.`,
 		tech: ["NextJs", "Typescript", "Tailwind"],
 		features: [
-			"User Login & Signup functionality",
-			"Ineract with blogs or create your own",
+			"<b>Faster Load Times:</b> Utilized Next.js features like server-side rendering <b>(SSR)</b> and static site generation <b>(SSG)</b> to deliver faster page load times, enhancing user satisfaction and retention.",
 		],
 		imgSrc: onScreenMovieImage,
 		deployedLink: "https://onscreen-movie.vercel.app/",
 		gitLink: "https://github.com/lokesh-dc/moviesearch",
 	},
 	{
-		title: "Organize.todo",
+		title: "Organize.tasks",
 		desc: `A dynamic project management tool designed to empower users to organize their tasks efficiently. It features a fully customizable to-do list with multiple statuses (e.g., To Do, In Review, Done) and the ability to define additional statuses to suit user workflows.`,
 		tech: [
 			"NextJs",
@@ -43,8 +42,8 @@ export const data = [
 		desc: `A responsive weather application using HTML, JavaScript, and CSS that provides real-time weather updates. The app enables users to view current weather conditions for their hometown or any searched location. Additionally, it integrates an interactive map to monitor the current location, assisting users in identifying potential nature-related traffic or abnormalities.`,
 		tech: ["HTML", "Weather API", "CSS", "Javascript"],
 		features: [
-			"User Login & Signup functionality",
-			"Ineract with blogs or create your own",
+			"Location based weather",
+			"Search for location to get latest weather",
 		],
 		imgSrc: weatherIO,
 		deployedLink: "https://hilarious-alpaca-f7c2a1.netlify.app",
